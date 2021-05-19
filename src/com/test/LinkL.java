6a98216440b60c4b2f6664b59a3e6d459037d8b5
hasanshaih <hafizov.marat@mail.ru>
2021-05-20 00:13:09 +0300

    Более правильный вариант

--- src/com/test/LinkL.java
+++ src/com/test/LinkL.java
@@ -7,13 +7,15 @@ import java.util.ListIterator;
 public class LinkL {
     public static void main(String[] args) {
         List<Integer> list = new LinkedList<>();
-        ListIterator<Integer> iterator = list.listIterator(); //подключение ListIterator
+        ListIterator<Integer> iterator = list.listIterator();
+        iterator.add(0);
         for (int i = 0; i < 20; i++) {
             iterator.add(i + 1);
             if (i % 2 == 0) {
-                iterator.previous();//возвращаемся на шаг назад
+                iterator.previous();
             }
         }
         System.out.println(list);
     }
 }
+
